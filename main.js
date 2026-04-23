0d5138cf7132eec1eb28497316dd0e2c2b9ce076
khamkhunaung310-code <khamkhunaung310@gmail.com>
2026-04-23 16:59:26 +0900

    My first comment

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// const name = prompt("Please enter your name:");
+// alert("Hello, " + name + "! Welcome to our website.");
+
+const target = document.getElementById("target");
+target.textContent = "Hello ! Welcome to our website.";
+target.style.color = "blue";
+target.style.fontSize = "24px";
+target.style.backgroundColor = "lightgray";
