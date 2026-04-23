375d906ce176b6f9eae877fcbb4e60455b7e6de9
khamkhunaung310-code <khamkhunaung310@gmail.com>
2026-04-23 17:08:06 +0900

    MY first comment

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ const target = document.getElementById("target");
 target.textContent = "Hello ! Welcome to our website.";
 target.style.color = "blue";
 target.style.fontSize = "24px";
-target.style.backgroundColor = "lightgray";
+target.style.backgroundColor = "lightgray"; 
