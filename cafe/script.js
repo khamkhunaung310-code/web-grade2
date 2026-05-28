b6dc0e22852a9068d0beddea37f9252c197ba95f
khamkhunaung310-code <khamkhunaung310@gmail.com>
2026-05-28 15:56:48 +0900

    ifelse

--- cafe/script.js
+++ cafe/script.js
@@ -1,9 +1,21 @@
 // form
 function reserve() {
+    // 入力した内容を取得する
     const name = document.getElementById("guestName").value;
     const count = document.getElementById("guestCount").value;
 
+    // 結果を表示する場所を取得する
     const result = document.getElementById("reserveResult");
 
-    result.textContent = `✓ ご予約ありがとうございます、${name}様。${count}名様で承りました。`;
+     // 名前か人数が空だったら、注意メッセージを出して終了する
+   if (name === '' || count === '') {
+       result.textContent = '入力して下さい。';
+   } else {
+       result.textContent = `✓ ご予約ありがとうございます、${name}様。${count}名様で承りました。`;
+   }
+}
+
+// recommend`
+function pickRecommend() {
+
 }
